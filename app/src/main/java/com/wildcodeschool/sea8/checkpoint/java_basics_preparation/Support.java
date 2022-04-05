b3f38d6f4b77a382ed0a194f6f638768e46f852f
Miles Lynam-Smith <miles.lynam-smith@telekom.de>
2022-04-05 14:47:02 +0200

    support certifications added

--- app/src/main/java/com/wildcodeschool/sea8/checkpoint/java_basics_preparation/Support.java
+++ app/src/main/java/com/wildcodeschool/sea8/checkpoint/java_basics_preparation/Support.java
@@ -11,13 +11,17 @@ public class Support extends ITPerson {
 
     @Override
     public boolean hasReadAccess() {
-        // Add logic to evaluate Read access for Support here.
-        return false;
+        if (level > 1)
+            return true;
+        else
+            return false;
     }
 
     @Override
     public boolean hasWriteAccess() {
-        // Add logic to evaluate Write access for Admin here.
-        return false;
+        if (level >= 2)
+        return true;
+            else 
+                return false;
     }
 }
