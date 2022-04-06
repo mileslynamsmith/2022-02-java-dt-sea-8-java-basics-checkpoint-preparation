6e112e5595861a4d523bb2db08f7897dbf6508f5
Miles Lynam-Smith <miles.lynam-smith@telekom.de>
2022-04-06 14:42:59 +0200

    ITPersonRepair

--- app/src/main/java/com/wildcodeschool/sea8/checkpoint/java_basics_preparation/ITPersonRepair.java
+++ app/src/main/java/com/wildcodeschool/sea8/checkpoint/java_basics_preparation/ITPersonRepair.java
@@ -6,7 +6,9 @@ public class ITPersonRepair {
         // Get the name of the ITPerson, repair it and return the new name
         // EG: person.getName() -> "Super_Admin" -> return "Super Admin"
         // get name 
-        person.getName() 
-        return "";
+        // what do i know?  I know that getName in ITPerson can be used for other classes.  I know that the ITPerson is a class, and person is another parametre
+        // i need to create a String // then make that string do all the work 
+        String name = person.getName().replace("_", " ");
+        return name;
     }
 }
