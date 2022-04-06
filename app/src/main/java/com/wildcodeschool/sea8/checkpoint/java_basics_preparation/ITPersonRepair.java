93afe6d5042e9e0fa6ebf91b5a2ac266569bd0a7
Miles Lynam-Smith <miles.lynam-smith@telekom.de>
2022-04-06 12:59:30 +0200

    quiz answers

--- app/src/main/java/com/wildcodeschool/sea8/checkpoint/java_basics_preparation/ITPersonRepair.java
+++ app/src/main/java/com/wildcodeschool/sea8/checkpoint/java_basics_preparation/ITPersonRepair.java
@@ -5,6 +5,8 @@ public class ITPersonRepair {
     public static String repairName(ITPerson person) {
         // Get the name of the ITPerson, repair it and return the new name
         // EG: person.getName() -> "Super_Admin" -> return "Super Admin"
+        // get name 
+        person.getName() 
         return "";
     }
 }
